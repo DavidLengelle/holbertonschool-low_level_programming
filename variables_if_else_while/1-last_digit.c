3bc488e1b10fd469a820cdf96614f13d3512d0cd
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-24 15:35:08 +0100

    fix: Correct last digit variable usage in 1-last_digit.c for accurate output

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,17 +19,17 @@ int main(void)
 
 	l_digit = n % 10;
 
-	if (n > 5)
+	if (l_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", l_digit, n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 	}
-	else if (n == 0)
+	else if (l_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", l_digit, n);
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", l_digit, n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
 	}
 return (0);
 }
