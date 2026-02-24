9ad19d91ff12f73cf0f68a0b20ba92ab9c631f2e
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-24 15:32:46 +0100

    fix: Correct last digit variable usage in 1-last_digit.c for accurate output

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,33 +2,34 @@
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
 	int n;
-	int x;
+	int l_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 
-x = n % 10;
+	l_digit = n % 10;
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+		printf("Last digit of %d is %d and is greater than 5\n", l_digit, n);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", l_digit, n);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", l_digit, n);
 	}
 return (0);
 }
