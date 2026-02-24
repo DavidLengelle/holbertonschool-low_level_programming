def3cbe2cbc61b641bcfd66d8702de00fbe9463c
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-24 11:39:24 +0100

    fix: Add missing semicolons and include stdio.h in 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -13,16 +14,17 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive")
+		printf("is positive");
 	}
+
 	if (n < 0)
 	{
-		printf("is negative")
+		printf("is negative");
 	}
 
 	if (n == 0)
 	{
-		printf("is zero")
+		printf("is zero");
 	}
 	/* your code goes there */
 	return (0);
