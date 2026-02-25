8ccc458edb48989b513234bd5617f042d9decd42
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-25 19:31:22 +0100

    feat: Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int H, m;
+
+	for (H = 0; H <= 23; H++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
