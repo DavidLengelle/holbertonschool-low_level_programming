f1ef40735382b325df7a02074d092fb5fc58374f
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-25 15:26:10 +0100

    fix: write loop for the repeat ten times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,15 @@
 
 void print_alphabet_x10(void)
 {
+int boucle;
 char lettre;
+
+for (boucle = 0; boucle < 10; boucle++)
+{
 for (lettre = 'a'; lettre <= 'z'; lettre++)
 {
 	_putchar(lettre);
 }
 _putchar('\n');
-
+}
 }
