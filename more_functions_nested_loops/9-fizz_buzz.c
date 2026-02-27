1b0100c9917bba22117291e7c939dcbabedc28a6
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-27 20:42:31 +0100

    feat: The "Fizz-Buzz test"

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints numbers 1-100, replaces multiples of 3 with Fizz,
+ * multiples of 5 with Buzz and multiples of both with FizzBuzz.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int num;
+
+for (num = 1; num <= 100; num++)
+{
+if (num % 3 == 0 && num % 5 == 0)
+{
+    printf("FizzBuzz");
+}
+else if (num % 3 == 0)
+{
+    printf("Fizz");
+}
+else if (num % 5 == 0)
+{
+    printf("Buzz");
+}
+else
+{
+printf("%d", num);
+}
+
+if (num < 100)
+{
+			printf(" ");
+}
+}
+return (0);
+}
