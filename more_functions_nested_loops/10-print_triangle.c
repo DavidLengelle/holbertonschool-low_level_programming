f3bd3f039dc10b0a56a091b89563cbf39c3d9dc9
David Lengellé <david.lengelle.1.10@gmail.com>
2026-02-27 21:24:53 +0100

    feat: print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *@size: is the size of the triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+int line, space, sharp;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (line = 1; line <= size; line++)
+		{
+			for (space = 0; space < (size - line); space++)
+			{
+				_putchar(' ');
+			}
+
+			for (sharp = 0; sharp < line; sharp++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+}
