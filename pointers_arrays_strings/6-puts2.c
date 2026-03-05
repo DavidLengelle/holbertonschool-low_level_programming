bedb9fd255239cd858cd86d969f83b54ea6e651c
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-05 13:13:40 +0100

    fix: add NULL

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * puts2 - prints every other character of a string,
@@ -11,10 +12,21 @@ void puts2(char *str)
 {
 	int i = 0;
 
+if (str == NULL)
+{
+_putchar('\n');
+return;
+}
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
+
+if (str[i - 1] == '\0')
+{
+break;
+}
 	}
 	_putchar('\n');
 }
