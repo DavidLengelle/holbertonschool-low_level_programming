b99afa0b03cfe4e8d2bd364d488fb17a04e40930
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-05 14:53:40 +0100

    feat: first try

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,4 +10,28 @@
 int _atoi(char *s)
 {
 
+int i = 0;
+int res = 0;
+int sign = 1;
+int found = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+found = 1;
+
+
+}
+else if (found)
+{
+break;
+}
+i++;
+}
+
+return (res * sign);
 }
