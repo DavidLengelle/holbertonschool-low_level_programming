0f1db2ca59a8de5c3e9186e7d1689a37f439c2dc
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-06 22:27:45 +0100

    feat: Write a program that adds positive numbers.

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+/**
+ * main - adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
