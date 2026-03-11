6c9a0dd752cf1b1e5bf4b9c01aac20c02c84a4f6
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-11 23:40:04 +0100

    fix: return

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers.
+ * @min: nb minimum
+ * @max: nb maximum
+ * Return: A pointer to the allocated memory.
+ */
+
+int *array_range(int min, int max)
+{
+
+}
