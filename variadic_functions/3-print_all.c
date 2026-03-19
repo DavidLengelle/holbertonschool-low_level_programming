f0e163057e1e22a6fc37c43c4178debe5cedbe11
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-19 23:12:03 +0100

    refactor: Simplify print_all function by replacing if statements with switch case

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -23,28 +23,27 @@ void print_all(const char * const format, ...)
 
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
-			printf("%s%c", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-			separator = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", separator, str);
-			separator = ", ";
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				separator = ", ";
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				separator = ", ";
+				break;
 		}
 		i++;
 	}
