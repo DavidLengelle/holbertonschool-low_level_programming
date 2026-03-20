c3c59fd97905802ea3aaaae8b6a2f24d09e99397
David Lengellé <david.lengelle.1.10@gmail.com>
2026-03-20 14:24:11 +0100

    feat: Add _strlen_recursion function to calculate the length of a string recursively

--- recursion/main.h
+++ recursion/main.h
@@ -47,5 +47,6 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif
