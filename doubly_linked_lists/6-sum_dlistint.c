1206dbd22c2109f35f4a030f7b401fc7fabf8be3
David Lengellé <david.lengelle.1.10@gmail.com>
2026-04-01 18:35:24 +0200

    fix: Correct formatting of the documentation comment in sum_dlistint function

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - returns the sum of all the data (n)-
+ * -of a dlistint_t linked list.
  * @head: pointer to the head of the list
  *
  * Return: sum
