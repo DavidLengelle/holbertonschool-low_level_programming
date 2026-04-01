68f44c64d784c6f99c217c1ee49705e30905851c
David Lengellé <david.lengelle.1.10@gmail.com>
2026-04-01 17:41:05 +0200

    feat: Implement free_dlistint function to free a doubly linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free a memory list
+ * @head: pointer of start list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+
+		free(head->n);
+		free(head);
+
+		head = temp;
+	}
+}
