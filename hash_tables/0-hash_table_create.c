c7ad4a1f645c2d7198430372fda80f0544d7d1ae
David Lengellé <david.lengelle.1.10@gmail.com>
2026-04-02 18:18:58 +0200

    feat: Implement hash_table_create function to create a new hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it fails.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+		if (ht == NULL)
+			return (NULL);
+
+	ht->size = size;
+
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
+
+}
